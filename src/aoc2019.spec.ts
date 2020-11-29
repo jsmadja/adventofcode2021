cf2060659d207697634b5cfda4469eca504d9722
Julien Smadja <julien.smadja@gmail.com>
2020-11-29 17:25:25 +0100

    :zap:

--- src/aoc2019.spec.ts
+++ src/aoc2019.spec.ts
@@ -123,12 +123,14 @@ describe('Day 1', () => {
       [1969, 654],
       [100756, 33583],
     ];
+
     each(testData).test(
       'For a mass of %d, the fuel required is %d',
       (mass, requiredFuel) => {
         expect(calculateRequiredFuel(mass)).toBe(requiredFuel);
       },
     );
+
     test('should compute full required fuel', () => {
       expect(
         input
@@ -144,16 +146,19 @@ describe('Day 1', () => {
      is 0, which would call for a negative fuel), so the total fuel required is still just 2`, () => {
       expect(calculateTrueRequiredFuel(14)).toBe(2);
     });
+
     test(`At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2).216
        then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no furtherfuel. So, the
         total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.`, () => {
       expect(calculateTrueRequiredFuel(1969)).toBe(966);
     });
+
     test(`The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 
     43 + 12 + 2 = 50346.
 `, () => {
       expect(calculateTrueRequiredFuel(100756)).toBe(50346);
     });
+
     test('should compute true full required fuel', () => {
       expect(
         input
