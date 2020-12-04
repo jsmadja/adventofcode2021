92df618c050ef0ef510c566a65d9aefa0e9fd973
jsmadja <julien.smadja@gmail.com>
2020-12-04 13:00:07 +0100

    :sparkles: Day 4 Puzzle 2

--- src/2020/day4/aoc2020.day4.spec.ts
+++ src/2020/day4/aoc2020.day4.spec.ts
@@ -12,6 +12,19 @@ interface Passport {
   hgt?: string;
 }
 
+const validPassport = {
+  ecl: 'gry',
+  pid: '860033327',
+  eyr: 2020,
+  hcl: '#fffffd',
+  byr: 1937,
+  iyr: 2017,
+  hgt: '183cm',
+  cid: 350
+};
+
+const EYE_COLORS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+
 function createPassportFrom(passportAsString: string) {
   return passportAsString
     .split('\n')
@@ -28,16 +41,59 @@ function createPassportFrom(passportAsString: string) {
     }, {} as any);
 }
 
+const CM = 'cm';
+
+const IN = 'in';
+
 function isValid(passport: Passport) {
-  return (
-    !!passport.ecl &&
-    !!passport.pid &&
-    !!passport.eyr &&
-    !!passport.hcl &&
-    !!passport.byr &&
-    !!passport.iyr &&
-    !!passport.hgt
-  );
+  if (
+    !passport.ecl ||
+    !passport.pid ||
+    !passport.eyr ||
+    !passport.hcl ||
+    !passport.byr ||
+    !passport.iyr ||
+    !passport.hgt
+  ) {
+    return false;
+  }
+
+  if (passport.byr < 1920 || passport.byr > 2002) {
+    return false;
+  }
+  if (passport.iyr < 2010 || passport.iyr > 2020) {
+    return false;
+  }
+  if (passport.eyr < 2020 || passport.eyr > 2030) {
+    return false;
+  }
+  if (!passport.hgt.endsWith(CM) && !passport.hgt.endsWith(IN)) {
+    return false;
+  }
+  if (
+    passport.hgt.endsWith(CM) &&
+    (+passport.hgt.split(CM)[0] < 150 || +passport.hgt.split(CM)[0] > 193)
+  ) {
+    return false;
+  }
+  if (
+    passport.hgt.endsWith(IN) &&
+    (+passport.hgt.split(IN)[0] < 59 || +passport.hgt.split(IN)[0] > 76)
+  ) {
+    return false;
+  }
+  const matchedColor = passport.hcl.match('#[0-9a-f]{6}');
+  if (!matchedColor || matchedColor[0] !== passport.hcl) {
+    return false;
+  }
+  if (EYE_COLORS.indexOf(passport.ecl) < 0) {
+    return false;
+  }
+  const matchedPid = passport.pid.match('[0-9]{9}');
+  if (!matchedPid || matchedPid[0] !== passport.pid) {
+    return false;
+  }
+  return true;
 }
 
 describe('Day 4', () => {
@@ -119,12 +175,72 @@ hcl:#cfa07d byr:1929`;
         .filter(isValid);
       expect(length).toBe(2);
     });
-    test('should find puzzle 1 answer', () => {
+    test.skip('should find puzzle 1 answer', () => {
       const { length } = puzzle1Input
         .split('\n\n')
         .map(createPassportFrom)
         .filter(isValid);
       expect(length).toBe(204);
+    });
+  });
+  describe('Part 2', () => {
+    test('Valid passport should have a byr between 1920 and 2002 ', () => {
+      expect(isValid({ ...validPassport, byr: 1919 })).toBe(false);
+      expect(isValid({ ...validPassport, byr: 1920 })).toBe(true);
+      expect(isValid({ ...validPassport, byr: 2002 })).toBe(true);
+      expect(isValid({ ...validPassport, byr: 2003 })).toBe(false);
+    });
+    test('Valid passport should have an iyr between 2010 and 2020 ', () => {
+      expect(isValid({ ...validPassport, iyr: 2009 })).toBe(false);
+      expect(isValid({ ...validPassport, iyr: 2010 })).toBe(true);
+      expect(isValid({ ...validPassport, iyr: 2020 })).toBe(true);
+      expect(isValid({ ...validPassport, iyr: 2021 })).toBe(false);
+    });
+    test('Valid passport should have an eyr between 2020 and 2030 ', () => {
+      expect(isValid({ ...validPassport, eyr: 2019 })).toBe(false);
+      expect(isValid({ ...validPassport, eyr: 2020 })).toBe(true);
+      expect(isValid({ ...validPassport, eyr: 2030 })).toBe(true);
+      expect(isValid({ ...validPassport, eyr: 2031 })).toBe(false);
+    });
+    test('Valid passport should have a valid hgt ', () => {
+      expect(isValid({ ...validPassport, hgt: '149cm' })).toBe(false);
+      expect(isValid({ ...validPassport, hgt: '150cm' })).toBe(true);
+      expect(isValid({ ...validPassport, hgt: '190cm' })).toBe(true);
+      expect(isValid({ ...validPassport, hgt: '193cm' })).toBe(true);
+      expect(isValid({ ...validPassport, hgt: '194cm' })).toBe(false);
+
+      expect(isValid({ ...validPassport, hgt: '58in' })).toBe(false);
+      expect(isValid({ ...validPassport, hgt: '59in' })).toBe(true);
+      expect(isValid({ ...validPassport, hgt: '60in' })).toBe(true);
+      expect(isValid({ ...validPassport, hgt: '76in' })).toBe(true);
+      expect(isValid({ ...validPassport, hgt: '77in' })).toBe(false);
+      expect(isValid({ ...validPassport, hgt: '190in' })).toBe(false);
+
+      expect(isValid({ ...validPassport, hgt: '190' })).toBe(false);
+    });
+    test('Valid passport should have a hair color # followed by exactly six characters 0-9 or a-f', () => {
+      expect(isValid({ ...validPassport, hcl: 'a' })).toBe(false);
+      expect(isValid({ ...validPassport, hcl: '#000fff' })).toBe(true);
+      expect(isValid({ ...validPassport, hcl: '#123abc' })).toBe(true);
+      expect(isValid({ ...validPassport, hcl: '#123abg' })).toBe(false);
+    });
+    test('Valid passport should have an eye color exactly one of: amb blu brn gry grn hzl oth', () => {
+      expect(isValid({ ...validPassport, ecl: 'a' })).toBe(false);
+      EYE_COLORS.forEach((ecl) =>
+        expect(isValid({ ...validPassport, ecl })).toBe(true)
+      );
+    });
+    test('Valid passport should have a pid a nine-digit number, including leading zeroes', () => {
+      expect(isValid({ ...validPassport, pid: 'a' })).toBe(false);
+      expect(isValid({ ...validPassport, pid: '000000001' })).toBe(true);
+      expect(isValid({ ...validPassport, pid: '0123456789' })).toBe(false);
+    });
+    test('should find puzzle 2 answer', () => {
+      const { length } = puzzle1Input
+        .split('\n\n')
+        .map(createPassportFrom)
+        .filter(isValid);
+      expect(length).toBe(179);
     });
   });
 });
