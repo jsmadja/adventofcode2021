591cc981293302a8f17d81047000df83b65a9529
jsmadja <julien.smadja@gmail.com>
2020-12-06 10:00:02 +0100

    :sparkles: Day 6 Puzzle 2

--- src/2020/day6/aoc2020.day6.spec.ts
+++ src/2020/day6/aoc2020.day6.spec.ts
@@ -13,6 +13,23 @@ function getYesAnswerCountOfForms(forms: string) {
     .reduce((a, b) => a + b, 0);
 }
 
+function getOnlyAllYesAnswerCount(allYesAnswers: string) {
+  const forms = allYesAnswers.split('\n');
+  const allLetters = forms.join('').split('');
+  return _(allLetters)
+    .countBy((a) => a)
+    .values()
+    .filter((l) => l === forms.length)
+    .value().length;
+}
+
+function getOnlyYesAnswerCountOfForms(forms: string) {
+  return forms
+    .split('\n\n')
+    .map(getOnlyAllYesAnswerCount)
+    .reduce((a, b) => a + b, 0);
+}
+
 describe('Day 6', () => {
   describe('Part 1', () => {
     test('should pass acceptance tests', () => {
@@ -42,6 +59,36 @@ b`;
     });
     test('should get puzzle 1 answer', () => {
       expect(getYesAnswerCountOfForms(puzzle1Input)).toBe(6596);
+    });
+  });
+  describe('Part 2', () => {
+    test('should pass acceptance tests', () => {
+      expect(
+        getOnlyAllYesAnswerCount(`abcx
+abcy
+abcz`)
+      ).toBe(3);
+    });
+    test('should sum all forms', () => {
+      const forms = `abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`;
+      expect(getOnlyYesAnswerCountOfForms(forms)).toBe(6);
+    });
+    test('should get puzzle 2 answer', () => {
+      expect(getOnlyYesAnswerCountOfForms(puzzle1Input)).toBe(3219);
     });
   });
 });
